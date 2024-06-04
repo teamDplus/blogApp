7da2ffa5f2175361ce525f199d07f3658924c554
teamDplus <satotaro199946@gmail.com>
2024-06-05 00:30:13 +0900

    header作成

--- src/index.js
+++ src/index.js
@@ -4,25 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppProvider } from './context/AppContext';
-import Header from './components/Header';
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <div className='contents'>
-        <div className='header'>
-          <Header/>
-        </div>
-        <div className='app'>
-          <App/>
-        </div>
-      </div>
+      <App/>
     </AppProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
