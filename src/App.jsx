869331f26e1d2112f5df1caf6be8f39d4c5091cd
kaiki uezawa <kaiki0202k@gmail.com>
2024-06-05 16:32:35 +0900

    急遽PostList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
-import './App.css';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Mypage from './components/Mypage';
-import Login from './components/Login';
-import Header from './components/Header';
-import Signup from './components/Signup';
-import Post from "./components/Post";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Mypage from "./components/Mypage";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Signup from "./components/Signup";
+import Post from "./components/PostList";
 
 function App() {
   return (
@@ -21,7 +20,7 @@ function App() {
             <Route path="/:id" element={<Mypage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/:id/posts/:postId" element={<Post />} />
-               <Route path="/signup" element={<Signup />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </div>
