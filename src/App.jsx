5e6e412fea963343bb0c42de1c05105e255c0c0b
kaiki uezawa <kaiki0202k@gmail.com>
2024-06-05 14:48:26 +0900

    記事表示機能作成

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Mypage from "./components/Mypage";
 import Login from "./components/Login";
+import Post from "./components/Post";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Mypage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/:id/posts/:postId" element={<Post />} />
       </Routes>
     </Router>
   );
