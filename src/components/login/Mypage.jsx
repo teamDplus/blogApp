155c9e8082d9548e3fd6c9d9cd2e2d6177482a08
kaiki uezawa <kaiki0202k@gmail.com>
2024-06-13 20:57:49 +0900

    検索機能追加

--- src/components/login/Mypage.jsx
+++ src/components/login/Mypage.jsx
@@ -38,7 +38,8 @@ const Mypage = () => {
   // Firebaseの中にあるpostsのフィールドから、ユーザーの投稿記事を取得
   useEffect(() => {
     // postsの中にあるコレクションの中からフィールドのauthorIdとログインしているuserと同じidの記事を取得
-    const q = query(collection(db, "posts"), where("authorId", "==", user.uid));
+    //postsの中にあるisDraftがfalseを取得(公開済みの記事)
+    const q = query(collection(db, "posts"), where("authorId", "==", user.uid), where("isDraft", "==", false));
 
     const unsubscribe = onSnapshot(q, (snapshot) => {
       const postsData = [];
@@ -65,12 +66,14 @@ const Mypage = () => {
         <h2>ブログ一覧</h2>
         <div className="mypage-list__items">
           {posts.map((post) => (
-            <Link to={`/${user.uid}/posts/${post.id}`} className="mypage-list__link">
-              <div key={post.id} className="mypage-list__item">
-                <h3 className="mypage-list__item-title">{post.title}</h3>
-                <p className="mypage-list__item-content">{post.content}</p>
-              </div>
-            </Link>
+            <div key={post.id}>
+              <Link to={`/${user.uid}/posts/${post.id}`} className="mypage-list__link">
+                <div className="mypage-list__item">
+                  <h3 className="mypage-list__item-title">{post.title}</h3>
+                  <p className="mypage-list__item-content">{post.content}</p>
+                </div>
+              </Link>
+            </div>
           ))}
         </div>
       </div>
